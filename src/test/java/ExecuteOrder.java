e09d5f6d0191e9117f11f579440d530a33dd0b64
mari-cherk <1mari.cherkasova@gmail.com>
2019-08-19 20:48:00 +0200

    Initial commit

---
+++ src/test/java/ExecuteOrder.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+import java.io.File;
+import java.sql.DriverManager;
+
+public class ExecuteOrder {
+
+
+    public static WebDriver getDriver(String browser) {
+        switch (browser) {
+            case "firefox":
+                System.setProperty(
+                        "webdriver.gecko.driver",
+                        new File(DriverManager.class.getResource("/geckodriver.exe").getFile()).getPath());
+                return new FirefoxDriver();
+            case "ie":
+            case "internet explorer":
+                System.setProperty(
+                        "webdriver.ie.driver",
+                        new File(DriverManager.class.getResource("/IEDriverServer.exe").getFile()).getPath());
+                return new InternetExplorerDriver();
+            case "chrome":
+            default:
+                System.setProperty(
+                        "webdriver.chrome.driver",
+                        new File(DriverManager.class.getResource("/chromedriver.exe").getFile()).getPath());
+                return new ChromeDriver();
+}
